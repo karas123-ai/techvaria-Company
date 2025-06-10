9f9eb9387b29bdc973aef5cefbfb4be7a74bffef
karas123-ai <karasezzat97@gmail.com>
2025-06-10 19:23:43 +0300

    Update script.js

--- index.html/script.js
+++ index.html/script.js
@@ -1,10 +1,9 @@
-
 function openGame(url) {
   document.getElementById("gameFrame").src = url;
-  document.getElementById("popup").style.display = "flex";
+  document.getElementById("popup").style.display = "block";
 }
 
 function closeGame() {
-  document.getElementById("gameFrame").src = "";
   document.getElementById("popup").style.display = "none";
+  document.getElementById("gameFrame").src = "";
 }
